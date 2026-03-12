0a85abec11d10dd08555c9cfa9524e3e9565dcdc
Sherlock Huang <baihan.huang@gmail.com>
2026-03-12 15:49:39 -0700

    Update on "Fix set_stack_trace"
    [ghstack-poisoned]

--- torch/fx/traceback.py
+++ torch/fx/traceback.py
@@ -291,6 +291,8 @@ def set_stack_trace(stack: list[str]):
         if stack:
             current_meta["stack_trace"] = "".join(stack)
         else:
+            # when the stack is empty, we explicitly clear the stack_trace to avoid
+            # propagating it to future node.˙
             current_meta.pop("stack_trace", None)
 
 
