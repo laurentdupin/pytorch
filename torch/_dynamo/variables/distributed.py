200022df2102caf179cc5a59ecbbbd64198b6fca
Simon Fan <xmfan@meta.com>
2026-03-13 03:52:58 +0000

    Update
    [ghstack-poisoned]

--- torch/_dynamo/variables/distributed.py
+++ torch/_dynamo/variables/distributed.py
@@ -31,6 +31,7 @@ from ..exc import unimplemented
 from ..external_utils import call_module_hooks_from_backward_state
 from ..guards import GuardBuilder, install_guard
 from ..source import AttrSource
+from ..utils import istype
 from .base import VariableTracker
 
 
