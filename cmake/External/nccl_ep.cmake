5cd4fd41ae1ac9aa99432d022330016a0343e9c5
Ke Wen <kwen@nvidia.com>
2026-03-13 23:07:44 +0000

    Update
    [ghstack-poisoned]

---
+++ cmake/External/nccl_ep.cmake
@@ -0,0 +1,9 @@
+if(NOT __NCCL_EP_INCLUDED)
+  set(__NCCL_EP_INCLUDED TRUE)
+
+  message(STATUS "Configuring NCCL EP as third-party dependency (__caffe2_nccl_ep)")
+  add_library(__caffe2_nccl_ep INTERFACE)
+  add_dependencies(__caffe2_nccl_ep nccl_external)
+  target_link_libraries(__caffe2_nccl_ep INTERFACE ${NCCL_EP_LIBRARIES})
+  target_include_directories(__caffe2_nccl_ep INTERFACE ${NCCL_EP_INCLUDE_DIRS})
+endif()
