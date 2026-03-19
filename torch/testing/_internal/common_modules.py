85e8c0786a5a095c70e1d1039c1fb1bfc3960ae7
gplutop7 <grzegorzx.pluto-prondzinski@intel.com>
2026-03-19 09:03:53 +0000

    xpu: mark ConvTranspose2d complex32 parity as expected failure

--- torch/testing/_internal/common_modules.py
+++ torch/testing/_internal/common_modules.py
@@ -3885,6 +3885,8 @@ module_db: list[ModuleInfo] = [
                    # Not implemented for chalf on CPU
                    DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_cpu_gpu_parity',
                                 dtypes=(torch.chalf,), device_type='cuda'),
+                   DecorateInfo(unittest.expectedFailure, 'TestModule', 'test_cpu_gpu_parity',
+                                dtypes=(torch.chalf, torch.complex32), device_type='xpu'),
                ),
                decorators=(
                    DecorateInfo(precisionOverride({torch.float32: 1e-04}), 'TestModule', 'test_memory_format'),
