e0b74c8c86c3e75160d76fcad6c6c5b54b9c4748
Xu Han <xu.han@outlook.com>
2026-03-24 15:58:16 +0800

    fix lint

--- aten/src/ATen/native/mkldnn/xpu/GroupedBlas.cpp
+++ aten/src/ATen/native/mkldnn/xpu/GroupedBlas.cpp
@@ -14,12 +14,13 @@ Tensor _grouped_mm_xpu(
   _grouped_mm_validate_inputs(mat_a, mat_b, offs, bias, out_dtype);
   const auto out_dtype_ =
       _resolve_grouped_mm_out_dtype(mat_a, mat_b, out_dtype);
-  Tensor out = create_grouped_gemm_output_tensor(mat_a, mat_b, offs, out_dtype_);
+  Tensor out =
+      create_grouped_gemm_output_tensor(mat_a, mat_b, offs, out_dtype_);
 
-  bool use_fast_path = (mat_a.dtype() == at::kBFloat16 &&
-                        mat_b.dtype() == at::kBFloat16 &&
-                        out_dtype_ == at::kBFloat16 &&
-                        at::native::xpu::is_grouped_mm_available());
+  bool use_fast_path =
+      (mat_a.dtype() == at::kBFloat16 && mat_b.dtype() == at::kBFloat16 &&
+       out_dtype_ == at::kBFloat16 &&
+       at::native::xpu::is_grouped_mm_available());
   if (use_fast_path) {
     at::native::xpu::bf16bf16_grouped_mm(mat_a, mat_b, offs, bias, out);
   } else {
