8c3fa1e45416c123aef41975c6543613eb7e5114
William Wen <williamwen@meta.com>
2026-03-24 12:53:04 -0700

    [dynamo, nested graph breaks] enable nested_graph_breaks on dynamo tests; remove TestCaseWithNestedGraphBreaks
    ghstack-source-id: c04eb2778958bbcb5dfa1ccac302fa7c7dafe797
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167695

--- test/dynamo/test_decorators.py
+++ test/dynamo/test_decorators.py
@@ -254,7 +254,7 @@ class DecoratorTests(PytreeRegisteringTestCase):
         fn(torch.randn(10))
 
         # Check for graph break
-        self.assertEqual(cnts.frame_count, 3)
+        self.assertEqual(cnts.frame_count, 2)
 
     def test_incorrect_usage_disallow_in_graph(self):
         with self.assertRaisesRegex(RuntimeError, "disallow_in_graph is expected"):
@@ -661,15 +661,16 @@ class DecoratorTests(PytreeRegisteringTestCase):
 
     def test_nonstrict_trace_no_action_at_a_distance(self):
         def trace_me(x):
+            x = x + 4
             torch._dynamo.graph_break()
-            return x + 42
+            return x + 8
 
         # No effect on traceability of `trace_me`
         torch._dynamo.nonstrict_trace(trace_me)
 
         def fn(x):
-            res = trace_me(x)
-            return res + 1
+            res = trace_me(x + 1)
+            return res + 2
 
         x = torch.randn(10)
         cnts = torch._dynamo.testing.CompileCounterWithBackend("aot_eager")
@@ -2021,7 +2022,7 @@ Detected recompile when torch.compile stance is 'fail_on_recompile'. filename: '
 
         inp = torch.ones(3)
         self.assertEqual(f5(inp), inp + 7)
-        self.assertEqual(cnts.frame_count, 4)
+        self.assertEqual(cnts.frame_count, 2)
 
         def inner_f6(x):
             x = x + 2
@@ -2037,7 +2038,7 @@ Detected recompile when torch.compile stance is 'fail_on_recompile'. filename: '
 
         cnts.clear()
         self.assertEqual(f6(inp), inp + 7)
-        self.assertEqual(cnts.frame_count, 3)
+        self.assertEqual(cnts.frame_count, 2)
 
         def inner_f7(x):
             x = x + 2
@@ -2077,7 +2078,7 @@ Detected recompile when torch.compile stance is 'fail_on_recompile'. filename: '
 
         inp = torch.ones(3)
         self.assertEqual(f8(inp), inp + 7)
-        self.assertEqual(cnts.frame_count, 4)
+        self.assertEqual(cnts.frame_count, 3)
 
         def inner2_f9(x):
             x = x + 2
@@ -2140,7 +2141,7 @@ Detected recompile when torch.compile stance is 'fail_on_recompile'. filename: '
 
         inp = torch.ones(3)
         self.assertEqual(f1(inp), inp + 7)
-        self.assertEqual(cnts.frame_count, 4)
+        self.assertEqual(cnts.frame_count, 2)
 
         def inner1_f2(x):
             x = x + 1
@@ -2260,6 +2261,7 @@ Detected recompile when torch.compile stance is 'fail_on_recompile'. filename: '
 
         self.assertEqual(cnts.frame_count, 0)
 
+    @torch._dynamo.config.patch(nested_graph_breaks=False)
     def test_nested_compile_fullgraph(self):
         # Test that fullgraph=True cannot be toggled back by fullgraph=False
         inp = torch.ones(3)
